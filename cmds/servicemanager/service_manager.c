719944e003cf76d9f17664442afc5a6898b8183b
Jani Monoses <jani@ubuntu.com>
2015-05-07 18:03:42 +0300

    Remove permission checks in service manager.
    Change-Id: I91e07e0a118059eae0a1db1cc27a26649f6b498b

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -18,50 +18,6 @@
 #include <cutils/log.h>
 #endif
 
-/* TODO:
- * These should come from a config file or perhaps be
- * based on some namespace rules of some sort (media
- * uid can register media.*, etc)
- */
-static struct {
-    unsigned uid;
-    const char *name;
-} allowed[] = {
-    { AID_MEDIA, "media.audio_flinger" },
-    { AID_MEDIA, "media.log" },
-    { AID_MEDIA, "media.player" },
-    { AID_MEDIA, "media.camera" },
-    { AID_MEDIA, "media.audio_policy" },
-    { AID_MEDIA, "media.nvidia.audio_alsa" },
-#ifdef YAMAHAPLAYER
-    { AID_MEDIA, "media.yamahaplayer" },
-#endif
-#ifdef SECTVOUT
-    { AID_MEDIA, "SecTVOutService" },
-#endif
-    { AID_DRM,   "drm.drmManager" },
-    { AID_NFC,   "nfc" },
-    { AID_BLUETOOTH, "bluetooth" },
-    { AID_RADIO, "radio.phone" },
-    { AID_RADIO, "radio.sms" },
-    { AID_RADIO, "radio.phonesubinfo" },
-    { AID_RADIO, "radio.simphonebook" },
-/* TODO: remove after phone services are updated: */
-    { AID_RADIO, "phone" },
-    { AID_RADIO, "sip" },
-    { AID_RADIO, "isms" },
-    { AID_RADIO, "iphonesubinfo" },
-    { AID_RADIO, "simphonebook" },
-    { AID_RADIO, "phone_msim" },
-    { AID_RADIO, "isms_msim" },
-    { AID_RADIO, "iphonesubinfo_msim" },
-    { AID_RADIO, "simphonebook_msim" },
-    { AID_MEDIA, "common_time.clock" },
-    { AID_MEDIA, "common_time.config" },
-    { AID_KEYSTORE, "android.security.keystore" },
-    { AID_MEDIA, "listen.service" }
-};
-
 void *svcmgr_handle;
 
 const char *str8(uint16_t *x)
@@ -86,20 +42,6 @@ int str16eq(uint16_t *a, const char *b)
     if (*a || *b)
         return 0;
     return 1;
-}
-
-int svc_can_register(unsigned uid, uint16_t *name)
-{
-    unsigned n;
-    
-    if ((uid == 0) || (uid == AID_SYSTEM))
-        return 1;
-
-    for (n = 0; n < sizeof(allowed) / sizeof(allowed[0]); n++)
-        if ((uid == allowed[n].uid) && str16eq(name, allowed[n].name))
-            return 1;
-
-    return 0;
 }
 
 struct svcinfo 
@@ -174,12 +116,6 @@ int do_add_service(struct binder_state *bs,
 
     if (!ptr || (len == 0) || (len > 127))
         return -1;
-
-    if (!svc_can_register(uid, s)) {
-        ALOGE("add_service('%s',%p) uid=%d - PERMISSION DENIED\n",
-             str8(s), ptr, uid);
-        return -1;
-    }
 
     si = find_svc(s, len);
     if (si) {
